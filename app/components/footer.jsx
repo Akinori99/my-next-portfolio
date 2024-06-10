15945c6fc826772fdf32c5fa150826addb5c9638
Akinori99 <akinori.work99@gmail.com>
2024-06-11 01:11:35 +0900

    ✨ Create <Footer>

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer>
-      <p>@{new Date().getFullYear()} Abe Akinori</p>
+    <footer className="mt-6 bottom-0 w-full bg-black text-white text-center py-3">
+      <p>&copy;{new Date().getFullYear()} Abe Akinori</p>
     </footer>
   );
 }
