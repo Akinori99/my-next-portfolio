b6450031b8ac3f926ff461e1f0bea0a2b137bcf0
Akinori99 <akinori.work99@gmail.com>
2024-06-10 18:36:31 +0900

    ♻️ Change extension & <Header><Footer> loading

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/app/components/header';
+import Footer from '@/app/components/footer';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: {
+    template: `%s | AbeAkinori's_Portfolio`,
+    default: `AbeAkinori's_Portfolio`,
+  },
+  description: `This is AbeAkinori's portfolio website`,
+  metadataBase: new URL(
+    'https://my-next-portfolio-akinori99s-projects.vercel.app'
+  ),
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ja">
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
