583981aabd1ae57de843f050367c2e5097adffc8
Akinori99 <akinori.work99@gmail.com>
2024-06-24 21:41:38 +0900

    ♻️ <Footer> fixed at bottom of screen

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mt-6 bottom-0 w-full bg-black text-white text-center py-3">
+    <footer className="mt-6 fixed bottom-0 w-full bg-black bg-opacity-90 text-white text-center py-3 z-50">
       <p>&copy;{new Date().getFullYear()} Abe Akinori</p>
     </footer>
   );
