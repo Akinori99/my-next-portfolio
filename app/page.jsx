ad3cee51f533a7a9196fee038885e2994d9b560a
Akinori99 <akinori.work99@gmail.com>
2024-06-24 23:07:32 +0900

    📱 Make <button> responsive

--- app/page.jsx
+++ app/page.jsx
@@ -10,9 +10,9 @@ export default function TOP() {
           Portfolio
         </h1>
       </div>
-      <div className="mt-10">
+      <div className="mt-20">
         <Link href="/works">
-          <button className="inline-flex items-center justify-center text-white text-lg sm:text-xl rounded-lg px-8 py-4 transition-transform duration-300 transform-gpu hover:translate-y-[-2px] bg-gradient-to-r from-[#003b6f] to-[#0076b3] shadow-custom focus:outline-none focus:shadow-focus">
+          <button className="inline-flex items-center justify-center text-white text-lg sm:text-xl lg:text-2xl xl:text-3xl rounded-lg px-8 py-4 sm:px-10 sm:py-5 transition-transform duration-300 transform-gpu hover:translate-y-[-2px] bg-gradient-to-r from-[#003b6f] to-[#0076b3] shadow-custom focus:outline-none focus:shadow-focus">
             作品を見る
           </button>
         </Link>
