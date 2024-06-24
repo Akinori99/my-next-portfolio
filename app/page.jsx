c3979d10a0a4a8bf2bba644a4206acb65ca37bb9
Akinori99 <akinori.work99@gmail.com>
2024-06-24 22:50:46 +0900

    📱 Make <h1> responsive

--- app/page.jsx
+++ app/page.jsx
@@ -2,19 +2,21 @@ import Link from 'next/link';
 
 export default function TOP() {
   return (
-    <div className="mx-auto relative overflow-hidden min-h-screen px-[0.5vw]">
-      <div className="relative mt-[21vh] mb-0 leading-none text-azure">
-        <h1 className="text-[10rem] font-normal mb-0 text-shadow-custom md:text-[9rem] sm:text-[6rem] sm:tracking-[-3px]">
+    <div className="flex flex-col justify-center items-center min-h-screen px-5">
+      <div className="text-center leading-none text-azure w-full">
+        <h1 className="text-[16vw] md:text-[16vw] lg:text-[16vw] xl:text-[16vw] max-w-[calc(100vw-10px)] font-normal text-shadow-custom tracking-wide">
           Akinori&apos;s
           <br />
           Portfolio
         </h1>
       </div>
-      <Link href="/works">
-        <button className="inline-flex items-center justify-center text-white text-[1.5rem] rounded-[10px] px-[55px] py-[15px] mt-[50px] transition-transform duration-300 transform-gpu hover:translate-y-[-2px] bg-gradient-to-r from-[#003b6f] to-[#0076b3] shadow-custom focus:outline-none focus:shadow-focus">
-          作品を見る
-        </button>
-      </Link>
+      <div className="mt-10">
+        <Link href="/works">
+          <button className="inline-flex items-center justify-center text-white text-lg sm:text-xl rounded-lg px-8 py-4 transition-transform duration-300 transform-gpu hover:translate-y-[-2px] bg-gradient-to-r from-[#003b6f] to-[#0076b3] shadow-custom focus:outline-none focus:shadow-focus">
+            作品を見る
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
