ea9deba2179e6cb53a3e78e1b86f7423f1f992c7
Akinori99 <akinori.work99@gmail.com>
2024-07-08 21:06:56 +0900

    ♻️ CSS-related adjustments

--- app/components/wrapper.jsx
+++ app/components/wrapper.jsx
@@ -1,11 +1,16 @@
 export default function WRAPPER(props) {
+  const paddingTOP = props.isTopPage ? 'pt-20' : 'pt-72';
+  const paddingBottom = props.isTopPage ? 'pb-0' : 'pb-64';
+
   return (
     <div
       className="flex flex-col justify-center items-center min-h-screen px-5 bg-cover bg-center"
       style={{ backgroundImage: `url('/img/${props.img}')` }}
     >
       <div className="text-center leading-none text-azure w-full">
-        <h1 className="text-[16vw] md:text-[16vw] lg:text-[16vw] xl:text-[16vw] max-w-[calc(100vw-10px)] font-normal text-shadow-custom tracking-wide">
+        <h1
+          className={`${paddingTOP} ${paddingBottom} pt- text-[16vw] md:text-[16vw] lg:text-[16vw] xl:text-[16vw] max-w-[calc(100vw-10px)] font-normal text-shadow-custom tracking-wide`}
+        >
           {props.title}
         </h1>
         {props.children && <div>{props.children}</div>}
