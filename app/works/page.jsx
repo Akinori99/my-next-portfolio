8b0382a0b2772515d57b17ec2859cba1fd3e1bf5
Akinori99 <akinori.work99@gmail.com>
2024-07-23 20:55:26 +0900

    ♻️ WorkItem component creation and application & dynamic import for improved performance

--- app/works/page.jsx
+++ app/works/page.jsx
@@ -1,10 +1,15 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
+import { useEffect, useState, useCallback } from 'react';
+import dynamic from 'next/dynamic';
 import WRAPPER from '@/app/components/wrapper';
 import CONTAINER from '@/app/components/container';
 import BUTTON from '@/app/components/button';
+
+const WorkItem = dynamic(() => import('@/app/components/WorkItem'), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+});
 
 export default function WORKS() {
   const [workItems, setWorkItems] = useState([]);
@@ -23,58 +28,15 @@ export default function WORKS() {
     fetchData();
   }, []);
 
+  const renderWorkItem = useCallback((item) => (
+    <WorkItem key={item.title} item={item} />
+  ), []);
+
   return (
     <div>
       <WRAPPER img="works.jpg" title="WORKS">
         <CONTAINER>
-          {workItems.map((item) => (
-            <div
-              key={item.title}
-              className="bg-gray-200 rounded-lg shadow-md p-6 mt-6"
-            >
-              <a
-                href={item.siteURL}
-                className="block mb-4 w-full relative"
-                style={{ aspectRatio: '5 / 3' }}
-              >
-                <Image
-                  src={item.imgSrc}
-                  alt={item.title}
-                  fill
-                  className="object-contain"
-                />
-              </a>
-              <h2 className="text-2xl mb-4">{item.title}</h2>
-              <div className="text-left">
-                <p className="mb-4">
-                  {item.description} <br />
-                  {item.ReferenceURL && (
-                    <a href={item.ReferenceURL}>※ {item.ReferenceURL}</a>
-                  )}
-                </p>
-                <h3 className="text-xl border-l-4 border-blue-800 pl-4 mb-2">
-                  開発言語
-                </h3>
-                <p className="mb-4">{item.developmentLanguage}</p>
-                <h3 className="text-xl border-l-4 border-blue-800 pl-4 mb-2">
-                  サイトURL
-                </h3>
-                <p className="mb-4">
-                  <a href={item.siteURL}>{item.siteURL || item.siteURLnot}</a>
-                </p>
-                <h3 className="text-xl border-l-4 border-blue-800 pl-4 mb-2">
-                  コード
-                </h3>
-                <p className="mb-4">
-                  <a href={item.codeURL}>{item.codeURL || item.codeURLnot}</a>
-                </p>
-                <h3 className="text-xl border-l-4 border-blue-800 pl-4 mb-2">
-                  作成日
-                </h3>
-                <p dangerouslySetInnerHTML={{ __html: item.createdDate }}></p>
-              </div>
-            </div>
-          ))}
+          {workItems.map(renderWorkItem)}
         </CONTAINER>
         <BUTTON href="/about" txt="自己紹介へ" />
       </WRAPPER>
