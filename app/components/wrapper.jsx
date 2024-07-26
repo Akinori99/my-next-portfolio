e497abda9b45fdfc992f11c433da75f853bb6dfe
Akinori99 <akinori.work99@gmail.com>
2024-07-26 20:14:53 +0900

    ♻️ Background image modification (z-index)

--- app/components/wrapper.jsx
+++ app/components/wrapper.jsx
@@ -2,15 +2,13 @@ export default function WRAPPER({ isTopPage, img, title, children }) {
   const paddingClasses = isTopPage ? 'pt-20 pb-0' : 'pt-72 pb-64';
 
   return (
-    <div
-      className={`flex flex-col justify-center items-center min-h-screen px-5 bg-cover bg-center bg-fixed ${paddingClasses}`}
-      style={{
-        backgroundImage: `url('/img/${img}')`,
-        backgroundAttachment: 'fixed',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-      }}
-    >
+    <div className={`relative flex flex-col justify-center items-center min-h-screen px-5 ${paddingClasses}`}>
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-fixed z-[-1]"
+        style={{
+          backgroundImage: `url('/img/${img}')`,
+        }}
+      ></div>
       <div className="text-center leading-none text-azure w-full">
         <h1
           className={`text-[16vw] md:text-[16vw] lg:text-[16vw] xl:text-[16vw] max-w-[calc(100vw-10px)] font-normal text-shadow-custom tracking-wide`}
