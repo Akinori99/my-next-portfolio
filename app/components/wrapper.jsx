8037789849bd5cc63d8d14250dd41860c8c678ce
Akinori99 <akinori.work99@gmail.com>
2024-07-27 20:27:42 +0900

    ♻️ CSS fine tuning

--- app/components/wrapper.jsx
+++ app/components/wrapper.jsx
@@ -1,17 +1,17 @@
 export default function WRAPPER({ isTopPage, img, title, children }) {
-  const paddingClasses = isTopPage ? 'pt-20 pb-0' : 'pt-72 pb-64';
+  const paddingClasses = isTopPage ? 'pt-20 pb-0' : 'pt-64 pb-32';
 
   return (
-    <div className={`relative flex flex-col justify-center items-center min-h-screen px-5 ${paddingClasses}`}>
+    <div className={`relative flex flex-col justify-center items-center min-h-screen px-5`}>
       <div
-        className="absolute inset-0 bg-cover bg-center bg-fixed z-[-1]"
+        className="absolute inset-0 w-full h-full bg-cover bg-center bg-fixed z-[-1]"
         style={{
           backgroundImage: `url('/img/${img}')`,
         }}
       ></div>
       <div className="text-center leading-none text-azure w-full">
         <h1
-          className={`text-[16vw] md:text-[16vw] lg:text-[16vw] xl:text-[16vw] max-w-[calc(100vw-10px)] font-normal text-shadow-custom tracking-wide`}
+          className={`text-[16vw] md:text-[16vw] lg:text-[16vw] xl:text-[16vw] max-w-[calc(100vw-10px)] font-normal text-shadow-custom tracking-wide ${paddingClasses}`}
         >
           {title}
         </h1>
