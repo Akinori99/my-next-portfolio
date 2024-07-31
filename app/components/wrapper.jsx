f0396da98b03071a84c7af13f2ecb55b411540a6
Akinori99 <akinori.work99@gmail.com>
2024-07-31 11:21:35 +0900

    ♻️ Review of fixed background image code

--- app/components/wrapper.jsx
+++ app/components/wrapper.jsx
@@ -2,14 +2,17 @@ export default function WRAPPER({ isTopPage, img, title, children }) {
   const paddingClasses = isTopPage ? 'pt-20 pb-0' : 'pt-64 pb-32';
 
   return (
-    <div className={`relative flex flex-col justify-center items-center min-h-screen px-5`}>
+    <div className={`relative flex flex-col justify-center items-center min-h-screen px-5 overflow-hidden`}>
       <div
-        className="absolute inset-0 w-full h-full bg-cover bg-center bg-fixed z-[-1]"
+        className="absolute inset-0 bg-cover bg-center bg-fixed"
         style={{
           backgroundImage: `url('/img/${img}')`,
+          backgroundAttachment: 'fixed',
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
         }}
       ></div>
-      <div className="text-center leading-none text-azure w-full">
+      <div className="relative z-10 text-center leading-none text-azure w-full">
         <h1
           className={`text-[16vw] md:text-[16vw] lg:text-[16vw] xl:text-[16vw] max-w-[calc(100vw-10px)] font-normal text-shadow-custom tracking-wide ${paddingClasses}`}
         >
