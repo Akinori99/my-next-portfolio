d67e0ff530fed25f40a93ef21759c9caa19fb5fd
Akinori99 <akinori.work99@gmail.com>
2024-08-10 13:47:00 +0900

    ♻️ Refactoring and CSS adjustments

--- app/components/Section.js
+++ app/components/Section.js
@@ -1,26 +1,28 @@
 import { memo } from 'react';
 import SkillItem from './SkillItem';
 
-const Section = ({ title, items, isSkill }) => (
-  <div className="bg-gray-200 rounded-lg shadow-md p-6 md:col-span-2">
-    <h2 className="text-3xl font-bold">{title}</h2>
-    {items.map(item => (
-      <div key={item.title || item.name} className="mt-4">
-        {isSkill ? (
-          <SkillItem skill={item} />
-        ) : (
-          <>
-            <p>
-              <span className="font-bold underline">{item.title}</span>
-              <br />
-              {item.description}
-            </p>
-          </>
-        )}
-      </div>
-    ))}
-  </div>
-);
+const Section = ({ title, items, isSkill }) => {
+  const renderItem = item => (
+    <div key={item.title || item.name} className="mt-8">
+      {isSkill ? (
+        <SkillItem skill={item} />
+      ) : (
+        <p className="text-left">
+          <span className="font-bold underline mb-4 block">{item.title}</span>
+          <span>{item.description}</span>
+        </p>
+      )}
+    </div>
+  );
+
+  return (
+    <div className="bg-gray-200 rounded-lg shadow-md p-6 md:col-span-2">
+      <h2 className="text-3xl font-bold">{title}</h2>
+      {items.map(renderItem)}
+    </div>
+  );
+};
 
 Section.displayName = 'Section';
+
 export default memo(Section);
--- app/components/SkillItem.js
+++ app/components/SkillItem.js
@@ -1,21 +1,19 @@
 import { memo } from 'react';
 import Image from 'next/image';
 
-const SkillItem = ({ skill }) => (
-  <div className="flex flex-col md:flex-row items-start md:items-center mb-6">
-    <div className="flex items-center">
-      <Image src={skill.imgSrc} alt={skill.name} width={64} height={64} />
+const SkillItem = ({ skill: { imgSrc, name, rating, description } }) => (
+  <div className="flex flex-col items-start mb-12">
+    <div className="flex items-center mb-4">
+      <Image src={imgSrc} alt={name} width={64} height={64} />
       <div className="ml-4">
         <p className="font-bold">
-          {skill.name}
+          {name}
           <br />
-          <span className="text-yellow-600">{skill.rating}</span>
+          <span className="text-yellow-600">{rating}</span>
         </p>
       </div>
     </div>
-    <div className="mt-4 md:mt-0 md:ml-8">
-      <p>{skill.description}</p>
-    </div>
+    <p className="text-left">{description}</p>
   </div>
 );
 
