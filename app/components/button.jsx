10ea2de063ccc22a2d5e433a02102b365bb2394d
Akinori99 <akinori.work99@gmail.com>
2024-08-20 20:24:05 +0900

    ♻️ Move footer component below element & adjust margins on button component

--- app/components/button.jsx
+++ app/components/button.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function BUTTON(props) {
   return (
     <Link href={props.href}>
-      <button className="my-20 inline-flex items-center justify-center text-white text-lg sm:text-xl lg:text-2xl xl:text-3xl rounded-lg px-8 py-4 sm:px-10 sm:py-5 transition-transform duration-300 transform-gpu hover:translate-y-[-2px] bg-gradient-to-r from-[#003b6f] to-[#0076b3] shadow-custom focus:outline-none focus:shadow-focus">
+      <button className="my-14 inline-flex items-center justify-center text-white text-lg sm:text-xl lg:text-2xl xl:text-3xl rounded-lg px-8 py-4 sm:px-10 sm:py-5 transition-transform duration-300 transform-gpu hover:translate-y-[-2px] bg-gradient-to-r from-[#003b6f] to-[#0076b3] shadow-custom focus:outline-none focus:shadow-focus">
         {props.txt}
       </button>
     </Link>
