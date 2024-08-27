699e25eb450191eea13c25a30d3a28b66fb21366
Akinori99 <akinori.work99@gmail.com>
2024-08-27 20:57:51 +0900

    ♻️ Added Japanese explanation

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -7,7 +7,8 @@ const NotFound = () => {
   return (
     <div className="min-h-screen flex flex-col bg-black text-white">
       <main className="flex-grow flex flex-col items-center justify-center text-center">
-        <h1 className="text-4xl font-bold mb-12">404 | This page could not be found.</h1>
+        <h1 className="text-4xl font-bold mb-8">404 | This page could not be found.</h1>
+        <p className="mb-12 text-2xl">お探しのページは見つかりませんでした。</p>
         <div className="flex flex-wrap justify-center space-x-0 md:space-x-6">
           <div className="w-full md:w-auto flex justify-center">
             <BUTTON href="/works" txt="作品を見る" />
