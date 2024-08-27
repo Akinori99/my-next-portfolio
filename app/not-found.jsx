ca0dd3702b8775135419c3aecbbab89b78427088
Akinori99 <akinori.work99@gmail.com>
2024-08-27 20:24:38 +0900

    ✨ Not-found page creation

---
+++ app/not-found.jsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React from 'react';
+import BUTTON from '@/app/components/button';
+
+const NotFound = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-black text-white">
+      <main className="flex-grow flex flex-col items-center justify-center text-center">
+        <h1 className="text-4xl font-bold mb-12">404 | This page could not be found.</h1>
+        <div className="flex flex-wrap justify-center space-x-0 md:space-x-6">
+          <div className="w-full md:w-auto flex justify-center">
+            <BUTTON href="/works" txt="作品を見る" />
+          </div>
+          <div className="w-full md:w-auto flex justify-center">
+            <BUTTON href="/about" txt="自己紹介へ" />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default NotFound;
