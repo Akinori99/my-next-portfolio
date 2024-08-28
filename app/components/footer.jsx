8321507d7ef00e98a30b3c04b760230f3dd3b47b
Akinori99 <akinori.work99@gmail.com>
2024-08-28 14:15:43 +0900

    ♻️ Changed footer padding to py-2

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-black bg-opacity-90 text-white text-center py-3">
+    <footer className="bg-black bg-opacity-90 text-white text-center py-2">
       <p>&copy;{new Date().getFullYear()} Abe Akinori</p>
     </footer>
   );
